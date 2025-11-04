79d2f246efe0b516f98e533c59378e50cb1649c1
hsen1221 <abogzoan9@gmail.com>
2025-11-04 19:25:33 +0300

    added class Vault

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,22 @@
 public class Main {
-}
+
+
+    static class Vault {
+        private final int password;
+
+        public Vault() {
+            int randomPassword = (int) (Math.random() * 10000);
+            this.password = randomPassword;
+            System.out.println("Vault created - Password: " + this.password);
+        }
+
+
+        public boolean isCorrectPassword(int guess) {
+            try {
+                Thread.sleep(5); // Simulate processing time
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return guess == this.password;
+        }
+    }}
