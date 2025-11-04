6209ebd46eb393f298c95c2df19d45d8c11b5bf2
hsen1221 <abogzoan9@gmail.com>
2025-11-04 19:37:20 +0300

    added Police thread

--- src/Main.java
+++ src/Main.java
@@ -68,5 +68,21 @@ public class Main {
             }
         }
     }
+    static class PoliceThread extends Thread {
+        @Override
+        public void run() {
+            for (int i = 10; i > 0; i--) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Police arriving in: " + i + " seconds");
+            }
+
+            System.out.println("Game over for you hackers");
+            System.exit(0);
+        }
+    }
 
 }
