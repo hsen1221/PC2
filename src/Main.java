8cc7b153606fc4bdff28189495b3aa85ac06f230
hsen1221 <abogzoan9@gmail.com>
2025-11-04 19:30:41 +0300

    added Descending hacker thread

--- src/Main.java
+++ src/Main.java
@@ -53,4 +53,20 @@ public class Main {
         }
     }
 
+    static class DescendingHackerThread extends HackerThread {
+        public DescendingHackerThread(Vault vault) {
+            super(vault);
+            this.setName("DescendingHackerThread");
+        }
+
+        @Override
+        public void run() {
+            for (int guess = 9999; guess >= 0; guess--) {
+                if (vault.isCorrectPassword(guess)) {
+                    System.out.println(this.getName() + ": Hacked the vault! Password is " + guess);
+                    System.exit(0);
+                }
+            }
+        }
+    }
 }
