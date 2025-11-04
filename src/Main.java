68c07dc1c7cb1f56b9ef43981e0d211f5fb4a8b9
hsen1221 <abogzoan9@gmail.com>
2025-11-04 19:29:00 +0300

    added Abstracted class for the hackers

--- src/Main.java
+++ src/Main.java
@@ -13,10 +13,44 @@ public class Main {
 
         public boolean isCorrectPassword(int guess) {
             try {
-                Thread.sleep(5); // Simulate processing time
+                Thread.sleep(5);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             return guess == this.password;
         }
-    }}
+    }
+
+    static abstract class HackerThread extends Thread {
+        protected Vault vault;
+
+        public HackerThread(Vault vault) {
+            this.vault = vault;
+            this.setPriority(Thread.MAX_PRIORITY);
+        }
+
+        @Override
+        public void start() {
+            System.out.println("Starting " + this.getName());
+            super.start();
+        }
+    }
+
+    static class AscendingHackerThread extends HackerThread {
+        public AscendingHackerThread(Vault vault) {
+            super(vault);
+            this.setName("AscendingHackerThread");
+        }
+
+        @Override
+        public void run() {
+            for (int guess = 0; guess <= 9999; guess++) {
+                if (vault.isCorrectPassword(guess)) {
+                    System.out.println(this.getName() + ": Hacked the vault! Password is " + guess);
+                    System.exit(0);
+                }
+            }
+        }
+    }
+
+}
